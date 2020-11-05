9b2d1250b8f7197ddab61544fceb6d6e3bcfcb70
Fluffix <sanya_kulikov00@mail.ru>
2020-11-05 08:16:34 +0300

    commit

--- lib/pages/page3.dart
+++ lib/pages/page3.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
 
-
-class Pagethree extends StatelessWidget{
+class Pagethree extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Container(color: Colors.white);
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: CustomShape(),
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Colors.orange[300], Colors.red[400]])),
+      ),
+    );
+  }
+}
+
+class CustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    double height = size.height;
+    double wigth = size.width;
+    path.lineTo(0, height - 100);
+    path.quadraticBezierTo(wigth / 2, height, wigth, height - 100);
+    path.lineTo(wigth, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(oldClipper) {
+    return true;
   }
 }
