ad79b511d49ee4662b5101ca2b6cf29f6cce09a7
Fluffix <sanya_kulikov00@mail.ru>
2020-11-05 16:02:53 +0300

    Create Profile Page try three

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
 import 'package:food_app/pages/page1.dart';
 import 'package:food_app/pages/page2.dart';
 import 'package:food_app/pages/page3.dart';
@@ -14,18 +13,18 @@ class _homeState extends State<home> {
   int _curentIndex = 0;
   int pageindex;
 
-
   final List AppbarList = ['Рецепты', 'Тренер', 'Профиль'];
 
   Widget callpages(_curentIndex) {
     switch (_curentIndex) {
       case 0:
         return Pageone();
+        break;
       case 1:
         return Pagetwo();
+        break;
       case 2:
         return Pagethree();
-
         break;
       default:
     }
@@ -35,22 +34,15 @@ class _homeState extends State<home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-
       appBar: myAppBar(),
-
       body: callpages(_curentIndex),
-      
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-              // color: Colors.black12,
-              color: Color(0xFF4B1A39).withOpacity(0.2), 
-              blurRadius: 4, 
-            )
-          ]
-        ),
+        decoration: BoxDecoration(color: Colors.white, boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Color(0xFF4B1A39).withOpacity(0.2),
+            blurRadius: 4,
+          )
+        ]),
         child: Row(
           children: <Widget>[
             buildBottomBar('assets/icons/knife.svg', 30.15, 30.50, 0),
@@ -62,73 +54,71 @@ class _homeState extends State<home> {
     );
   }
 
- Widget buildBottomBar(String route,double height, double width, int index) {
+  Widget buildBottomBar(String route, double height, double width, int index) {
     return GestureDetector(
-          onTap: (){
-            setState(() {
-              _curentIndex = index;
-            });
-          },
-          child: Container( 
-            child: IconButton(
-              icon: SvgPicture.asset(
-                route, 
-                height: height, 
-                width: width, 
-                fit: BoxFit.fill,
-                color: (_curentIndex == index)? Colors.orange[900] : Colors.black,
-              ), 
-              onPressed: null
+      onTap: () {
+        setState(() {
+          _curentIndex = index;
+        });
+      },
+      child: Container(
+        child: IconButton(
+            icon: SvgPicture.asset(
+              route,
+              height: height,
+              width: width,
+              fit: BoxFit.fill,
+              color:
+                  (_curentIndex == index) ? Colors.orange[900] : Colors.black,
             ),
-            height: MediaQuery.of(context).size.height*0.07,
-            width: MediaQuery.of(context).size.width/3,
-            decoration: index == _curentIndex ? BoxDecoration(
-                color: index == _curentIndex ? Colors.green: Colors.white,
+            onPressed: null),
+        height: MediaQuery.of(context).size.height * 0.07,
+        width: MediaQuery.of(context).size.width / 3,
+        decoration: index == _curentIndex
+            ? BoxDecoration(
+                color: index == _curentIndex ? Colors.green : Colors.white,
                 border: Border(
                   bottom: BorderSide(width: 4, color: Colors.orange[800]),
                 ),
-                gradient:  LinearGradient(
-                  colors: [
-                    Colors.orange.withOpacity(0.3),
-                    Colors.orange.withOpacity(0.015)
-                  ],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter
-                ),  
-              ) : BoxDecoration(), 
-          ),
+                gradient: LinearGradient(colors: [
+                  Colors.orange.withOpacity(0.3),
+                  Colors.orange.withOpacity(0.015)
+                ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+              )
+            : BoxDecoration(),
+      ),
     );
   }
-
 
   AppBar myAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
       elevation: 0,
-
       flexibleSpace: Container(
         decoration: BoxDecoration(
-            boxShadow: (_curentIndex == 0)? <BoxShadow>[
-              BoxShadow(
-                color: Colors.black,
-                blurRadius: 4.0,
-              )] : <BoxShadow>[],
-              
-            gradient: LinearGradient(
-                colors: [Colors.orange[300], Colors.red[400]])),
+            boxShadow: (_curentIndex == 0)
+                ? <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 4.0,
+                    )
+                  ]
+                : <BoxShadow>[],
+            gradient:
+                LinearGradient(colors: [Colors.orange[300], Colors.red[400]])),
       ),
       title: Text(
         AppbarList[_curentIndex],
         style: TextStyle(
-          fontSize: 18,      
+          fontSize: 18,
           color: Colors.white,
           shadows: <Shadow>[
             Shadow(
-              offset: Offset(1.5 , 1.5 ),
+              offset: Offset(1.5, 1.5),
               blurRadius: 8.0,
               color: Color.fromARGB(125, 0, 0, 255),
             ),
-          ], 
+          ],
         ),
       ),
       // backgroundColor: Colors.orange[400],
--- lib/pages/page3.dart
+++ lib/pages/page3.dart
@@ -1,9 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-
-class Pagethree extends StatelessWidget{
+class Pagethree extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Container(color: Colors.white);
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 160,
+          child: Stack(children: <Widget>[
+            ClipPath(
+              clipper: CustomShape(),
+              child: Container(
+                height: 150,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.orange[300], Colors.red[400]])),
+              ),
+            ),
+            Center(
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.15,
+                width: MediaQuery.of(context).size.height * 0.15,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.white,
+                      width: MediaQuery.of(context).size.height * 0.001),
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/user_image.png')),
+                ),
+              ),
+            )
+          ]),
+        ),
+        SizedBox(height: 100),
+        Column(children: <Widget>[
+          buildContainer('assets/images/profile_star.png', 36.83, 37.09,
+              'Избранные блюда'),
+          SizedBox(height: 10),
+          buildContainer(
+              'assets/images/profile_darts.png', 37.19, 34.84, 'Моя цель'),
+          SizedBox(height: 10),
+          buildContainer('assets/images/profile_settings.png', 34.0, 34.0,
+              'Физические параметры'),
+        ])
+      ],
+    );
+  }
+
+  Container buildContainer(
+      String route, double width, double height, String textButton) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(7.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(color: Colors.black12, spreadRadius: 5, blurRadius: 8)
+          ]),
+      child: Container(
+        width: 400,
+        height: 75,
+        child: Row(
+          children: [
+            SizedBox(width: 30),
+            Image(
+              image: AssetImage(route),
+              width: width,
+              height: height,
+            ),
+            Container(
+                padding: EdgeInsets.only(left: 50),
+                child: Text(
+                  textButton,
+                  style: TextStyle(fontSize: 20),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    double height = size.height;
+    double wigth = size.width;
+    path.lineTo(0, height - 90);
+    path.quadraticBezierTo(wigth / 2, height, wigth, height - 90);
+    path.lineTo(wigth, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(oldClipper) {
+    return true;
   }
 }
